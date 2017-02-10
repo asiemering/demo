1ed6e42b0e2190ffb181e63e82e7023f715eca7d
audreyandoy <audreylandoy@gmail.com>
2017-02-09 17:10:30 -0800

    heroku pt 2

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// var express = require('express');
-// var path = require('path');
-// var app = express();
+var express = require('express');
+var path = require('path');
+var app = express();
 
-// app.use(express.static(path.join(__dirname,'views')));
-// app.get('/', function(req, res) {
-//   res.sendFile('index.html');
-// });
+app.use(express.static(path.join(__dirname,'views')));
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
 
 
-// app.listen(3000);
+app.listen(3000);
 
-var http = require('http');
 
-function onReq (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World');
-  res.end();
-}
 
 app.listen(process.env.PORT || 3000)
