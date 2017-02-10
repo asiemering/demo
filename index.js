7e0a9f5a11101538db655ada36e20e45b0f1a1a6
audreyandoy <audreylandoy@gmail.com>
2017-02-09 17:00:17 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// var express = require('express');
+// var path = require('path');
+// var app = express();
+
+// app.use(express.static(path.join(__dirname,'views')));
+// app.get('/', function(req, res) {
+//   res.sendFile('index.html');
+// });
+
+
+// app.listen(3000);
+
+var http = require('http');
+
+function onReq (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World');
+  res.end();
+}
+
+http.createServer(onReq).listen(8000);
